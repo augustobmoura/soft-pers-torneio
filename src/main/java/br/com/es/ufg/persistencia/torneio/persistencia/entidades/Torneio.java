cea523bfa596d479fa7d3e60d96d489fdd5f30af
augusto-moura <augusto.borgesm@gmail.com>
2019-06-26 19:08:51 -0300

    Adiciona mapeamento faltante

--- src/main/java/br/com/es/ufg/persistencia/torneio/persistencia/entidades/Torneio.java
+++ src/main/java/br/com/es/ufg/persistencia/torneio/persistencia/entidades/Torneio.java
@@ -2,9 +2,12 @@ package br.com.es.ufg.persistencia.torneio.persistencia.entidades;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -13,5 +16,13 @@ public class Torneio {
   @Id
   @GeneratedValue
   private Long id;
+
+  @NotNull
+  @NotBlank
+  @Column(columnDefinition = "text")
+  private String titulo;
+
+  @Column(columnDefinition = "text")
+  private String descricao;
 
 }
